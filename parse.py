56811b595b878866ef13348d0e6a0a2edb9ba30b
rafaelkoll <rafcall98@hotmail.com>
2022-04-03 22:51:46 +0100

    Examples 6 and 7

--- parse.py
+++ parse.py
@@ -1,7 +1,7 @@
 import nltk
 from nltk import FeatureChartParser
 
-fcfg = nltk.data.load('grammar5.fcfg')
+fcfg = nltk.data.load('grammar7.fcfg')
 parser = FeatureChartParser(fcfg)
 
 def parse_text(text):
